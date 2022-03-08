283c615e906b68b5ff53b6daddf882f0ff77ab20
NikolasTS <nikolai.tsa@blaize.tech>
2022-03-08 11:05:07 +0200

    bringing the project to the template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,14 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  env: {
-    browser: false,
-    es2021: true,
-    mocha: true,
-    node: true,
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
-  ],
+  root: true,
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
   },
-  rules: {
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      { ignores: ["modules"] },
-    ],
-  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
 };
