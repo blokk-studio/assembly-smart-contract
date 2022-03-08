18e5c01aef29ae3c6efdd19f73e1600eacf4b2f8
NikolasTS <nikolai.tsa@blaize.tech>
2022-03-08 11:08:25 +0200

    ADD | imports to contract

--- contracts/NFTMarketplace_v1.sol
+++ contracts/NFTMarketplace_v1.sol
@@ -1,111 +1,23 @@
-pragma solidity =0.8.0;
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/interfaces/IERC1155.sol";
+import "@openzeppelin/contracts/interfaces/IERC721.sol";
+import "@openzeppelin/contracts/interfaces/IERC1155Receiver.sol";
+import "@openzeppelin/contracts/interfaces/IERC721Receiver.sol";
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
-interface IERC721 {
-    function balanceOf(address owner) external view returns (uint256 balance);
-    function ownerOf(uint256 tokenId) external view returns (address owner);
-    function safeTransferFrom(address from, address to, uint256 tokenId) external;
-    function transferFrom(address from, address to, uint256 tokenId) external;
-    function approve(address to, uint256 tokenId) external;
-    function getApproved(uint256 tokenId) external view returns (address operator);
-    function setApprovalForAll(address operator, bool _approved) external;
-    function isApprovedForAll(address owner, address operator) external view returns (bool);
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
-    
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
-    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
-}
-
-interface ERC1155  {
-    event TransferSingle(address indexed _operator, address indexed _from, address indexed _to, uint256 _id, uint256 _value);
-    event TransferBatch(address indexed _operator, address indexed _from, address indexed _to, uint256[] _ids, uint256[] _values);
-    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
-    event URI(string _value, uint256 indexed _id);
-    function safeTransferFrom(address _from, address _to, uint256 _id, uint256 _value, bytes calldata _data) external;
-    function safeBatchTransferFrom(address _from, address _to, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external;
-    function balanceOf(address _owner, uint256 _id) external view returns (uint256);
-    function balanceOfBatch(address[] calldata _owners, uint256[] calldata _ids) external view returns (uint256[] memory);
-    function setApprovalForAll(address _operator, bool _approved) external;
-    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
-}
-
-interface IERC721Metadata is IERC721 {
-    function name() external view returns (string memory);
-    function symbol() external view returns (string memory);
-    function tokenURI(uint256 tokenId) external view returns (string memory);
-}
-
-interface IERC721Receiver {
-    function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) external returns (bytes4);
-}
-
-interface ERC1155TokenReceiver {
-    function onERC1155Received(address _operator, address _from, uint256 _id, uint256 _value, bytes calldata _data) external returns(bytes4);
-    function onERC1155BatchReceived(address _operator, address _from, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external returns(bytes4);       
-}
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "./utils/TransferHelper.sol";
 
 interface ILotTokensPool {
     function balanceOf(address account) external view returns (uint256);
     function lotToken() external view returns (IERC721);
 }
 
-library Address {
-    function isContract(address account) internal view returns (bool) {
-        uint size;
-        assembly { size := extcodesize(account) }
-        return size > 0;
-    }
-}
-
-contract Ownable {
-    address public owner;
-    address public newOwner;
-
-    event OwnershipTransferred(address indexed from, address indexed to);
-
-    constructor() {
-        owner = msg.sender;
-        emit OwnershipTransferred(address(0), owner);
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner, "Ownable: Caller is not the owner");
-        _;
-    }
-
-    function transferOwnership(address transferOwner) public onlyOwner {
-        require(transferOwner != newOwner);
-        newOwner = transferOwner;
-    }
-
-    function acceptOwnership() virtual public {
-        require(msg.sender == newOwner);
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-        newOwner = address(0);
-    }
-}
-
-contract ReentrancyGuard {
-    /// @dev counter to allow mutex lock with only one SSTORE operation
-    uint private _guardCounter;
-
-    constructor () {
-        // The counter starts at one to prevent changing it from zero to a non-zero
-        // value, which is a more expensive operation.
-        _guardCounter = 1;
-    }
-
-    modifier nonReentrant() {
-        _guardCounter += 1;
-        uint localCounter = _guardCounter;
-        _;
-        require(localCounter == _guardCounter, "ReentrancyGuard: reentrant call");
-    }
-}
-
-contract NFTMarketplace is ReentrancyGuard, Ownable, IERC721Receiver, ERC1155TokenReceiver  {
+contract NFTMarketplace is ReentrancyGuard, Ownable, IERC721Receiver, IERC1155Receiver  {
 
     enum LotStatus {
         Inactive,
@@ -170,6 +82,10 @@ contract NFTMarketplace is ReentrancyGuard, Ownable, IERC721Receiver, ERC1155Tok
         _;
     }
 
+    function supportsInterface(bytes4 interfaceId) public view virtual override (IERC165) returns (bool) {
+        return interfaceId == type(IERC1155Receiver).interfaceId;
+    }
+
     function onERC1155Received(address _operator, address _from, uint256 _id, uint256 _value, bytes calldata _data) external pure override returns(bytes4){
         // return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
         return 0xf23a6e61;
@@ -193,7 +109,7 @@ contract NFTMarketplace is ReentrancyGuard, Ownable, IERC721Receiver, ERC1155Tok
 
         if(isMultiple){
             require(amount > 0, "NFTMarketplace: Amount must be greater than zero");
-            ERC1155(token).safeTransferFrom(owner, address(this), tokenId, amount, "0x0");
+            IERC1155(token).safeTransferFrom(owner, address(this), tokenId, amount, "0x0");
         }else{
             IERC721(token).safeTransferFrom(owner, address(this), tokenId);
         }
@@ -248,11 +164,11 @@ contract NFTMarketplace is ReentrancyGuard, Ownable, IERC721Receiver, ERC1155Tok
 
         require(localLot.status == LotStatus.Active || localLot.status == LotStatus.Inactive, "NFTMatketplace: Lot cannot be canceled");
 
-        address finalRecipient;
-        if(newRecipient == address(0)) finalRecipient = msg.sender;
+        address finalRecipient = newRecipient;
+        if(newRecipient == address(0)) finalRecipient = localLot.owner; 
 
         if(localLot.isMultiple){
-            ERC1155(localLot.token).safeTransferFrom(address(this), finalRecipient, localLot.tokenId, localLot.totalSupply-localLot.sold, "0x0");          
+            IERC1155(localLot.token).safeTransferFrom(address(this), finalRecipient, localLot.tokenId, localLot.totalSupply - localLot.sold, "0x0");          
         }else{
             IERC721(localLot.token).safeTransferFrom(address(this), finalRecipient, localLot.tokenId);
         }
@@ -279,7 +195,6 @@ contract NFTMarketplace is ReentrancyGuard, Ownable, IERC721Receiver, ERC1155Tok
         localLot.status = LotStatus.Inactive;
         activeLotCount--;
         emit DeactivateLot(lotId);
-
     }
 
     function batchDeactivateLots(uint[] memory lotIds) external onlyOwner {
@@ -346,7 +261,7 @@ contract NFTMarketplace is ReentrancyGuard, Ownable, IERC721Receiver, ERC1155Tok
         uint totalPrice = amount * localLot.price; // over???
         require(totalPrice < msg.value, "NFTMarketplace: Not enought value");
 
-        ERC1155(localLot.token).safeTransferFrom(address(this), msg.sender, localLot.tokenId, amount, "0x0");
+        IERC1155(localLot.token).safeTransferFrom(address(this), msg.sender, localLot.tokenId, amount, "0x0");
         
         TransferHelper.safeTransferETH(recipient, totalPrice);
         
@@ -364,7 +279,6 @@ contract NFTMarketplace is ReentrancyGuard, Ownable, IERC721Receiver, ERC1155Tok
 
         emit SoldLot(lotId, localLot.tokenId, localLot.token, msg.sender, amount, localLot.price);
     }
-
 
     function isAvailableToken(address token) public view returns (bool){
         if(isTokenFromPool){
@@ -422,92 +336,12 @@ contract NFTMarketplace is ReentrancyGuard, Ownable, IERC721Receiver, ERC1155Tok
         
         if(isMultiple){
             require(amount > 0, "NFTMarketplace: Cannot rescue 0");
-            ERC1155(token).safeTransferFrom(address(this), to, tokenId, amount, "0x0");          
+            IERC1155(token).safeTransferFrom(address(this), to, tokenId, amount, "0x0");          
         }else{
             IERC721(token).safeTransferFrom(address(this), to, tokenId);
         }
 
         emit RescueToken(to, token, tokenId, isMultiple, amount);
-    }
-
-}
-
-library EnumerableSet {
-    struct Set {
-        bytes32[] _values;
-        mapping (bytes32 => uint) _indexes;
-    }
-
-    function _add(Set storage set, bytes32 value) private returns (bool) {
-        if (!_contains(set, value)) {
-            set._values.push(value);
-            set._indexes[value] = set._values.length;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    function _remove(Set storage set, bytes32 value) private returns (bool) {
-        uint valueIndex = set._indexes[value];
-
-        if (valueIndex != 0) { // Equivalent to contains(set, value)
-            uint toDeleteIndex = valueIndex - 1;
-            uint lastIndex = set._values.length - 1;
-            bytes32 lastvalue = set._values[lastIndex];
-            set._values[toDeleteIndex] = lastvalue;
-            set._indexes[lastvalue] = toDeleteIndex + 1; // All indexes are 1-based
-            set._values.pop();
-            delete set._indexes[value];
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    function _contains(Set storage set, bytes32 value) private view returns (bool) {
-        return set._indexes[value] != 0;
-    }
-
-    function _length(Set storage set) private view returns (uint) {
-        return set._values.length;
-    }
-
-    function _at(Set storage set, uint index) private view returns (bytes32) {
-        require(set._values.length > index, "EnumerableSet: index out of bounds");
-        return set._values[index];
-    }
-
-    struct UintSet {
-        Set _inner;
-    }
-
-    function add(UintSet storage set, uint value) internal returns (bool) {
-        return _add(set._inner, bytes32(value));
-    }
-
-    function remove(UintSet storage set, uint value) internal returns (bool) {
-        return _remove(set._inner, bytes32(value));
-    }
-
-    function contains(UintSet storage set, uint value) internal view returns (bool) {
-        return _contains(set._inner, bytes32(value));
-    }
-
-    function length(UintSet storage set) internal view returns (uint) {
-        return _length(set._inner);
-    }
-
-    function at(UintSet storage set, uint index) internal view returns (uint) {
-        return uint(_at(set._inner, index));
-    }
-}
-
-library TransferHelper {
-
-    function safeTransferETH(address to, uint value) internal {
-        (bool success,) = to.call{value:value}(new bytes(0));
-        require(success, "TransferHelper: ETH_TRANSFER_FAILED");
     }
 
 }
---
+++ contracts/utils/TransferHelper.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+library TransferHelper {
+    function safeTransferETH(address to, uint value) internal {
+        (bool success,) = to.call{value:value}(new bytes(0));
+        require(success, "TransferHelper: ETH_TRANSFER_FAILED");
+    }
+}
